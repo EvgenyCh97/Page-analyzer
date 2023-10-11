c0118364ea30da67fcf03706e5bd27464adee266
Евгений Чайка <chay67ka26@gmail.com>
2023-10-11 15:00:45 +0300

    add index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 
@@ -9,4 +9,4 @@ app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def main():
-    return 'Hello, World!'
+    return render_template('index.html')
