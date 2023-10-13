12eacb3287f26ec5c37c304635d11811b106c395
Евгений Чайка <chay67ka26@gmail.com>
2023-10-13 11:41:47 +0300

    complete step 4

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -44,18 +44,22 @@ def check_site():
                 flash('Страница уже существует', 'info')
             cursor.execute('SELECT id FROM urls WHERE name = %s', (name,))
             id = cursor.fetchone()['id']
-            return redirect(url_for('get_site', id=id), code=302)
+    return redirect(url_for('get_site', id=id), code=302)
 
 
 @app.route('/urls/<id>')
 def get_site(id):
     messages = get_flashed_messages(with_categories=True)
+    site = get_site_from_db(id)
     with psycopg2.connect(DATABASE_URL) as connection:
         with connection.cursor(
                 cursor_factory=psycopg2.extras.DictCursor) as cursor:
-            cursor.execute('SELECT * FROM urls WHERE id = %s', (id,))
-            site = cursor.fetchone()
-    return render_template('show.html', messages=messages, site=site)
+            cursor.execute(
+                'SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC',
+                (id,))
+            checks = cursor.fetchall()
+    return render_template('show.html', messages=messages, site=site,
+                           checks=checks)
 
 
 @app.route('/urls')
@@ -63,6 +67,34 @@ def get_urls():
     with psycopg2.connect(DATABASE_URL) as connection:
         with connection.cursor(
                 cursor_factory=psycopg2.extras.DictCursor) as cursor:
-            cursor.execute('SELECT * FROM urls ORDER BY id DESC')
+            cursor.execute('SELECT DISTINCT ON (urls.id) urls.id, name, '
+                           'url_checks.created_at, url_checks.status_code '
+                           'FROM urls LEFT JOIN url_checks '
+                           'ON urls.id = url_checks.url_id '
+                           'ORDER BY urls.id DESC, url_checks.id DESC;')
             sites = cursor.fetchall()
     return render_template('index.html', sites=sites)
+
+
+@app.route('/urls/<id>/checks', methods=['POST'])
+def get_check(id):
+    site = get_site_from_db(id)
+    url_id = site['id']
+    with psycopg2.connect(DATABASE_URL) as connection:
+        with connection.cursor(
+                cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            current_date = date.today().isoformat()
+            cursor.execute(
+                'INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s)',
+                (url_id, current_date))
+            connection.commit()
+            flash('Страница успешно проверена', 'success')
+    return redirect(url_for('get_site', id=id), code=302)
+
+
+def get_site_from_db(id):
+    with psycopg2.connect(DATABASE_URL) as connection:
+        with connection.cursor(
+                cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute('SELECT * FROM urls WHERE id = %s', (id,))
+            return cursor.fetchone()
