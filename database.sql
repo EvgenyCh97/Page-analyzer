62b835d935bdb8c817af65823896a509fe3e6971
Евгений Чайка <chay67ka26@gmail.com>
2023-10-18 11:25:01 +0300

    add DELETE CASCADE to url_checks (url_id)

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@ CREATE TABLE urls (
 
 CREATE TABLE url_checks (
 	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	url_id bigint REFERENCES urls (id) NOT NULL,
+	url_id bigint REFERENCES urls (id) ON DELETE CASCADE NOT NULL,
 	status_code integer NOT NULL,
 	h1 varchar(255),
 	title varchar(255),
