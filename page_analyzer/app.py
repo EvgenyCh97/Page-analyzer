5f5441241ceedb1cc15d8914cdf4dcde60abebae
Евгений Чайка <chay67ka26@gmail.com>
2023-10-18 14:58:07 +0300

    fix similar blocks of code in check_site func

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -27,14 +27,12 @@ def get_main_page():
 def check_site():
     url = request.form.get('url')
 
-    if not url:
-        flash('URL обязателен', 'danger')
-        messages = get_flashed_messages(with_categories=True)
-        return render_template('main.html', messages=messages), 422
-    if not validators.url(url):
-        flash('Некорректный URL', 'danger')
-        messages = get_flashed_messages(with_categories=True)
-        return render_template('main.html', messages=messages), 422
+    for pass_check, message in ((url, 'URL обязателен'),
+                                (validators.url(url), 'Некорректный URL')):
+        if not pass_check:
+            flash(message, 'danger')
+            messages = get_flashed_messages(with_categories=True)
+            return render_template('main.html', messages=messages), 422
 
     name = (urlparse(url)[0] + '://' + urlparse(url)[1]).lower()
     if not select_from_db('SELECT * FROM urls WHERE name = %s', (name,)):
