2fcea0ee0ea2dc40a03d6dbe5c6924db1e6a429f
Евгений Чайка <chay67ka26@gmail.com>
2023-10-24 09:50:18 +0300

    move connect creation to db.py methods, add make_connection decorator to app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,20 +1,31 @@
 import os
 from datetime import date
 
+import psycopg2
+import psycopg2.extras
 import requests
 import validators
 from dotenv import load_dotenv
 from flask import (Flask, flash, get_flashed_messages, redirect,
                    render_template, request, url_for)
 
-from . import data_handlers, db
-from .db import (add_url, get_url_checks, get_url_from_db,
-                 get_urls_list)
+from . import db, parser
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+def make_connection(database_url):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            with psycopg2.connect(database_url) as connection:
+                return func(connection, *args, **kwargs)
+        inner.__name__ = func.__name__
+        return inner
+    return wrapper
 
 
 @app.route('/')
@@ -24,7 +35,8 @@ def get_main_page():
 
 
 @app.route('/urls', methods=['POST'])
-def check_url():
+@make_connection(DATABASE_URL)
+def check_url(connection):
     url = request.form.get('url')
 
     for pass_check, message in ((url, 'URL обязателен'),
@@ -34,43 +46,47 @@ def check_url():
             messages = get_flashed_messages(with_categories=True)
             return render_template('main.html', messages=messages), 422
 
-    name = data_handlers.extract_name(url)
-    checking_result = db.find_url(name)
+    name = parser.extract_name(url)
+    checking_result = db.find_url(connection, name)
     if not checking_result:
         current_date = date.today().isoformat()
-        add_url(name, current_date)
+        db.add_url(connection, name, current_date)
         flash('Страница успешно добавлена', 'success')
-        url_id = db.find_url(name)[0]['id']
+        url_id = db.find_url(connection, name)['id']
     else:
         flash('Страница уже существует', 'info')
-        url_id = checking_result[0]['id']
+        url_id = checking_result['id']
     return redirect(url_for('get_url', id=url_id), code=302)
 
 
 @app.route('/urls/<int:id>')
-def get_url(id):
+@make_connection(DATABASE_URL)
+def get_url(connection, id):
     messages = get_flashed_messages(with_categories=True)
-    url = get_url_from_db(id)
-    checks = get_url_checks(id)
+    url = db.get_url_from_db(connection, id)
+    checks = db.get_url_checks(connection, id)
     return render_template('show.html', messages=messages, url=url,
                            checks=checks)
 
 
 @app.route('/urls')
-def get_urls():
-    urls = get_urls_list()
+@make_connection(DATABASE_URL)
+def get_urls(connection):
+    urls = db.get_urls_list(connection)
     return render_template('index.html', urls=urls)
 
 
 @app.route('/urls/<int:id>/checks', methods=['POST'])
-def run_check(id):
-    site = get_url_from_db(id)
+@make_connection(DATABASE_URL)
+def run_check(connection, id):
+    url = db.get_url_from_db(connection, id)
     try:
-        status_code, title, h1, description = data_handlers.parse_site(site)
+        status_code, title, h1, description = parser.parse_site(url)
     except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'danger')
     else:
         current_date = date.today().isoformat()
-        db.add_url_check(id, status_code, h1, title, description, current_date)
+        db.add_url_check(connection, id, status_code, h1, title, description,
+                         current_date)
         flash('Страница успешно проверена', 'success')
     return redirect(url_for('get_url', id=id), code=302)
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,38 +1,61 @@
-from .db_handlers import insert_into_db, select_from_db
+import psycopg2
+import psycopg2.extras
 
 
-def find_url(name):
-    return select_from_db('SELECT * FROM urls WHERE name = %s', (name,))
+def find_url(connection, name):
+    with connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute('SELECT * FROM urls WHERE name = %s', (name,))
+        return cursor.fetchone()
 
 
-def add_url(name, current_date):
-    insert_into_db('INSERT INTO urls (name, created_at) VALUES (%s, %s)',
-                   (name, current_date))
+def add_url(connection, name, current_date):
+    with connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s)',
+                       (name, current_date))
+        connection.commit()
 
 
-def get_url_from_db(id):
-    return select_from_db('SELECT * FROM urls WHERE id = %s', (id,))[0]
+def get_url_from_db(connection, id):
+    with connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute('SELECT * FROM urls WHERE id = %s', (id,))
+        return cursor.fetchone()
 
 
-def get_url_checks(id):
-    return select_from_db(
-        'SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC', (id,))
+def get_url_checks(connection, id):
+    with connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(
+            'SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC',
+            (id,))
+        return cursor.fetchall()
 
 
-def get_urls_list():
-    return select_from_db('SELECT DISTINCT ON (urls.id) urls.id, name, '
-                          'url_checks.created_at, url_checks.status_code '
-                          'FROM urls LEFT JOIN url_checks '
-                          'ON urls.id = url_checks.url_id '
-                          'ORDER BY urls.id DESC, url_checks.id DESC;')
+def get_urls_list(connection):
+    with connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(
+            '''SELECT DISTINCT ON (urls.id) urls.id, name,
+            url_checks.created_at, url_checks.status_code
+            FROM urls
+            LEFT JOIN url_checks ON urls.id = url_checks.url_id
+            ORDER BY urls.id DESC, url_checks.id DESC;''')
+        return cursor.fetchall()
 
 
-def add_url_check(id, status_code, h1, title, description, current_date):
-    insert_into_db('INSERT INTO url_checks (url_id, status_code, h1, '
-                   'title, description, created_at)'
-                   ' VALUES (%s, %s, %s, %s, %s, %s)',
-                   (id, status_code,
-                    h1.string if h1 else None,
-                    title.string if title else None,
-                    description['content'] if description else None,
-                    current_date))
+def add_url_check(connection, id, status_code, h1, title, description,
+                  current_date):
+    with connection.cursor(
+            cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(
+            '''INSERT INTO url_checks (url_id, status_code, h1,
+            title, description, created_at)
+            VALUES (%s, %s, %s, %s, %s, %s)''',
+            (id, status_code,
+             h1.string if h1 else None,
+             title.string if title else None,
+             description['content'] if description else None,
+             current_date))
+        connection.commit()
